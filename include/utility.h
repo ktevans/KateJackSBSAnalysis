c92ac64b11f5907a46eedc82e1cd34cf30890aba
Katherine Evans <89591378+ktevans@users.noreply.github.com>
2025-06-11 14:58:58 -0400

    Update utility.h

--- include/utility.h
+++ include/utility.h
@@ -12,7 +12,7 @@
 namespace utility{
 
   //hard-coded output directory
-  string out_dir_temp = "/work/halla/sbs/ewertz/nTPE_analysis/output";
+  string out_dir_temp = "/work/halla/sbs/ktevans/KateJackSBSAnalysis/output";
   TString output_directory = TString(out_dir_temp);
 
   //function to determine if what is found was a number or a string
